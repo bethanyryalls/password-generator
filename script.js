95a1e0cb8c9fbb258c95ce614f7781311bdb0167
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-01-09 10:52:40 +0000

    changing given character arrays to strings, asking the user which characters they want to use in their password

--- script.js
+++ script.js
@@ -88,6 +88,17 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+  // Change Character set arrays to strings
+  var upperCharString = upperCasedCharacters.toString();
+  var lowerCharString = lowerCasedCharacters.toString();
+  var specCharString = specialCharacters.toString();
+  var numCharString = numericCharacters.toString();
+
+  console.log(upperCharString);
+  console.log(lowerCharString);
+  console.log(specCharString);
+  console.log(numCharString);
+
 // Function to prompt user for password options
 function getPasswordOptions() {
   
@@ -102,15 +113,36 @@ function getRandom(arr) {
 // Function to generate password with user input
 function generatePassword() {
   // ask the user to enter how many characters they want their password to be
-  var userChar = prompt("How many characters would you like your password to be?");
+  var passwordLength = prompt("How many characters would you like your password to be?");
 
-  // check if the number is higher than 10
-  if (userChar < 10) {
-    userChar = prompt("Please enter a number greater than 10");
-  }
+  // create empty string to store the password
+  var password = "";
 
-  console.log(userChar);
+  // create empty string to store possible characters
+  var possibleChar = [];
+
+  // Ask user for which types of characters to include in the password
+  var includeSpecial = confirm("Do you want to include special characters in your password?");
+  var includeUpper = confirm("Do you want to include uppercase letters in your password?");
+  var includeLower = confirm("Do you want to include lowercase letters in your password?");
+  var includeNumber = confirm("Do you want to include numbers in your password?");
+
+  // Add characters to string if chosen
+
+  // Change Character set arrays to strings
+  upperCasedCharacters.toString();
+  lowerCasedCharacters.toString();
+  specialCharacters.toString();
+  numericCharacters.toString();
+
+  console.log(upperCasedCharacters);
+  console.log(lowerCasedCharacters);
+  console.log(specialCharacters);
+  console.log(numericCharacters);
+
 }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
